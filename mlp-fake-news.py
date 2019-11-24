a2e71ae1e14a45ef0325c09bab9f004914a7f6fb
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2019-11-24 15:32:29 -0300

    [UPD] Adicionando dados de validação do modelo

--- mlp-fake-news.py
+++ mlp-fake-news.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
-from matplotlib import pyplot
 from keras import backend
 
 
@@ -65,29 +64,29 @@ def createModelMLP(vector_dimension = 300):
         activation = 'sigmoid'
     ))
 
-    # Compilação do modelo
+    # Compilação do modelo com as métricas: R2, RMSE e MAPE
     model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy', rmse, 'mape'])
     return model, input_layer_quantity_neuron, hidden_layer_quantity_neuron, hidden_layer_quantity
 
 
-# Método responsável por realizar o treinamento da MLP
-def train(model, x_train, y_train, epochs = 150):
+# Método responsável por realizar o treinamento e validação da MLP
+def train(model, x_train, y_train, x_val, y_val, epochs = 150):
     # Treinamento do modelo
-    history = model.fit(x_train, y_train, epochs = epochs, batch_size = 10)
+    history = model.fit(x_train, y_train, epochs = epochs, batch_size = 10, validation_data = (x_val, y_val))
     return model, history
 
 
-# Método responsável por realizar o teste do modelo
-def test(model, x_test, y_test, x, y):
+# Método responsável por realizar o teste da MLP
+def test(model, x_test, y_test, x_data, y_data):
     # Avalia o modelo com os dados de teste
     loss, accuracy_model, rmse, mape = model.evaluate(x_test, y_test)
     
     # Gera as detecções se cada notícia é fake ou não
-    detections = model.predict(x)
+    detections = model.predict(x_data)
 
     # Ajusta as detecções
     rounded = [round(x[0]) for x in detections]
-    accuracy_detection = np.mean(rounded == y)
+    accuracy_detection = np.mean(rounded == y_data)
 
     return loss, accuracy_model, rmse, mape, accuracy_detection
 
@@ -96,11 +95,10 @@ def test(model, x_test, y_test, x, y):
 
 JSON_NAME = './dataset/processed-data-train.json'
 VECTOR_DIMENSION = 300
-TEST_SIZE = 0.33
 EPOCHS = 150
 
 execucaoInicio = time.time()
-print('### Fake News detection - Treinamento e teste da MLP\n')
+print('### Fake News detection - Treinamento, validação e teste da MLP\n')
 
 x, y = readJson(JSON_NAME)
 if ((x is None) or (y is None)):
@@ -108,12 +106,16 @@ if ((x is None) or (y is None)):
     print('Antes de treinar e testar a rede neural realize o tratamento dos dados! ')
     sys.exit()
 
-# Divisão dos dados para treinamento e teste
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33)
+# Divisão dos dados para: Treinamento -> 70%, validação -> 20% e teste -> 10%
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.3)
+x_val, x_test, y_val, y_test = train_test_split(x_val, y_val, test_size = 0.333333)
 
-# Treinamento e teste do modelo
+
+# Criação do modelo MLP
 model, input_layer_neuron, hidden_layer_neuron, hidden_layer_quantity = createModelMLP(VECTOR_DIMENSION)
-model, history = train(model, x_train, y_train, EPOCHS)
+# Treinamento e validação do modelo
+model, history = train(model, x_train, y_train, x_val, y_val, EPOCHS)
+# Teste do modelo
 loss, accuracy_model, rmse, mape, accuracy_detection = test(model, x_test, y_test, x, y)
 
 # Cálculo do tempo de execução
@@ -121,38 +123,46 @@ execucaoFim = time.time()
 tempoExecucao = (execucaoFim - execucaoInicio) / 60
 
 
-print("\n\n### Resultados: ")
+print("\n\n### Resultados do teste da rede: ")
+# Quanto menor a perda, mais próximas nossas previsões são dos rótulos verdadeiros.
 print("Loss: %.2f" % loss)
 print("R2: %.2f%%" % (accuracy_model * 100))
+print("R2 Detecções: %.2f%%" % (accuracy_detection * 100))
 print("MAPE: %.2f" % mape)
 print("RMSE: %.2f" % rmse)
-print("R2 Detecções: %.2f%%" % (accuracy_detection * 100))
 print("###")
 print("QTD registros: %i " % len(x))
 print("QTD registros treino: %i " % len(x_train))
+print("QTD registros validação: %i " % len(x_val))
 print("QTD registros teste: %i " % len(x_test))
-print("Dados de teste: %.2f%%" %(TEST_SIZE * 100))
 print("QTD Épocas: %i" % EPOCHS)
 print("QTD neurônios camada de entrada: %i" % input_layer_neuron)
 print("QTD neurônios camadas intermediárias: %i" % hidden_layer_neuron)
 print("QTD de camadas intermediárias: %i" % hidden_layer_quantity)
 print("Tempo de Execução: %.2f minutos" % tempoExecucao)
 
-# Apresentação dos gráficos
-pyplot.plot(history.history['rmse'])
-plt.title('Erro Médio Quadrático da Raiz - RMSE')
+
+# Apresentação dos gráficos de treinamento e validação da rede
+plt.plot(history.history['rmse'])
+plt.plot(history.history['val_rmse'])
+plt.title('RMSE - Treinamento e validação')
 plt.xlabel('Épocas')
 plt.ylabel('RMSE')
-pyplot.show()
+plt.legend(['Treinamento', 'Validação'], loc='upper left')
+plt.show()
 
-pyplot.plot(history.history['mape'])
-plt.title('Erro Médio Percentual Absoluto - MAPE')
+plt.plot(history.history['mape'])
+plt.plot(history.history['val_mape'])
+plt.title('MAPE')
 plt.xlabel('Épocas')
 plt.ylabel('MAPE')
-pyplot.show()
+plt.legend(['Treinamento', 'Validação'], loc='upper left')
+plt.show()
 
-pyplot.plot(history.history['accuracy'])
-plt.title('Coeficiente de Determinação - R2')
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('R2 - Treinamento e validação')
 plt.xlabel('Épocas')
-plt.ylabel('R2 Detecções')
-pyplot.show()
+plt.ylabel('R2')
+plt.legend(['Treinamento', 'Validação'], loc='upper left')
+plt.show()
